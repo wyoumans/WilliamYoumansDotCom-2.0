44d05a132b0a416085cbdc422c5252b3f448df8a
William Youmans <will@williamyoumans.com>
2013-10-25 18:25:36 -0600

    added easing plugin

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,13 +1,31 @@
 (function() {
-  var animationSpeed = 1000;
+  var animationSpeed = 1000
+    , easing = 'easeOutBack';
 
   if ($('.masthead')) {
     $('h1 span.top').animate({
       left: 0
-    }, animationSpeed, function() {
+    }, animationSpeed, easing, function() {
       $('h1 span.bottom').animate({
         right: 0
-      }, animationSpeed);
+      }, animationSpeed, easing);
     });
   }
 })();
+
+function inLeft() {
+
+}
+
+function outLeft() {
+
+}
+
+function inRight() {
+
+}
+
+function outRight() {
+
+}
+
