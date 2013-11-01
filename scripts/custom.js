b24dc696c34673776c5b9d12e70d6efa7e4f1ca4
William Youmans <will@williamyoumans.com>
2013-11-01 12:52:37 -0600

    cleaned up home animations

--- scripts/custom.js
+++ scripts/custom.js
@@ -2,11 +2,11 @@
 
 (function() {
   var animationSpeed = 1000,
-    beginPosition = '-2000px',
-    endPosition = 0,
-    easing = 'easeOutBack',
-    $leftText = $('h1 span.top'),
-    $rightText = $('h1 span.bottom');
+      beginPosition  = '-2000px',
+      endPosition    = 0,
+      easing         = 'easeOutBack',
+      $leftText      = $('h1 span.top'),
+      $rightText     = $('h1 span.bottom');
 
   var animation = {
     inLeft: function(cb) {
@@ -24,14 +24,16 @@
 
         $leftText.animate({
           left: beginPosition
-        }, animationSpeed, easing, cb);
-      }, 1000);
+        }, animationSpeed, easing);
+
+        cb();
+      }, 4000);
     },
 
     inRight: function(cb) {
       cb = cb || function() {};
 
-      $rightText.stop(true, true).animate({
+      $rightText.stop(true, false).animate({
         right: endPosition
       }, animationSpeed, easing, cb);
     },
@@ -39,12 +41,15 @@
     outRight: function(cb) {
       cb = cb || function() {};
 
-      setTimeout(function() {
+      $rightText.stop(true, false).animate({
+        right: beginPosition
+      }, animationSpeed, easing, cb);
+    },
 
-        $rightText.stop(true, true).animate({
-          right: beginPosition
-        }, animationSpeed, easing, cb);
-      }, 1000);
+    bottomUp: function() {
+      cb = cb || function() {};
+
+      cb();
     }
   };
 
