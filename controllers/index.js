44a77234d10a7888b4e244c78fd1d56485fefa97
William Youmans <will@williamyoumans.com>
2013-11-22 10:59:03 -0700

    stubbed out main pages

--- controllers/index.js
+++ controllers/index.js
@@ -6,12 +6,48 @@ var getHistory = require('lastfm-history')
 
 module.exports.init = function(app) {
   app.get('/', index);
+  app.get('/portfolio', portfolio);
+  app.get('/services', services);
+  app.get('/blog', blog);
+  app.get('/about', about);
+  app.get('/contact', contact);
+
   // app.get('/last', last);
 };
 
 function index(req, res) {
   res.render('index', {
     title: 'Home'
+  });
+}
+
+function portfolio(req, res) {
+  res.render('generic', {
+    title: 'Portfolio'
+  });
+}
+
+function services(req, res) {
+  res.render('generic', {
+    title: 'Services'
+  });
+}
+
+function blog(req, res) {
+  res.render('generic', {
+    title: 'Blog'
+  });
+}
+
+function about(req, res) {
+  res.render('generic', {
+    title: 'About William'
+  });
+}
+
+function contact(req, res) {
+  res.render('generic', {
+    title: 'Request a Quote'
   });
 }
 
@@ -35,7 +71,7 @@ function last(req, res) {
     console.log('err:', err);
   });
 
-  res.render('index', {
-    title: 'Last'
+  res.render('generic', {
+    title: 'Last FM'
   });
 }
