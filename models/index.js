dc8412501f1db9e35773b6dfe35ab611c6a8f3b4
William Youmans <will@williamyoumans.com>
2013-11-22 13:19:49 -0700

    now correctly loading mongoose models

--- models/index.js
+++ models/index.js
@@ -10,33 +10,29 @@ var fs         = require('fs')
 
 Mongoose.connect(config.mongoConnectionString);
 
-(function loadModels() {
+(function() {
 
-  fs.readdir(__dirname, function(err, files) {
-    if (err) {
-      return console.log(err);
+  var files = fs.readdirSync(__dirname);
+
+  files.forEach(function(file) {
+
+    var fullPath = __dirname + '/' + file;
+
+    if (/\.js$/.test(file) && file !== 'index.js') {
+      var filename = file.replace(/\.js$/, '');
+
+      var name = filename.split('-').map(function(word) {
+        return capitalize(word);
+      }).join('');
+
+      var schema = require(fullPath);
+
+      schema.plugin(timeStamps, {
+        index: true
+      });
+
+      models[name] = Mongoose.model(name, schema);
     }
-
-    files.forEach(function(file) {
-
-      var fullPath = __dirname + '/' + file;
-
-      if (/\.js$/.test(file) && file !== 'index.js') {
-        var filename = file.replace(/\.js$/, '');
-
-        var name = filename.split('-').map(function(word) {
-          return capitalize(word);
-        }).join('');
-
-        var schema = require(fullPath);
-
-        schema.plugin(timeStamps, {
-          index: true
-        });
-
-        models[name] = Mongoose.model(name, schema);
-      }
-    });
   });
 }());
 
