1132f2fc49cb0bf329f5abb3b93f10b5685556e7
William Youmans <will@williamyoumans.com>
2014-01-16 12:56:08 -0700

    allow for confrodo to look at the environment variable

--- config/index.js
+++ config/index.js
@@ -1,9 +1,24 @@
 'use strict';
 
+var defaults = {
+  "mongoConnectionString": "mongodb://localhost/williamyoumans",
+  "port": 3000,
+  "logToFile": true,
+  "lastfm": {
+    "key": process.env.LASTFM_KEY,
+    "secret": process.env.LASTFM_SECRET
+  },
+  "instagram": {
+    "key": process.env.INSTAGRAM_KEY,
+    "secret": process.env.INSTAGRAM_SECRET,
+    "token": process.env.INSTAGRAM_TOKEN,
+    "userid": process.env.INSTAGRAM_USERID
+  }
+};
+
 var confrodo = require('confrodo')
-  , common   = __dirname + '/common.json'
   , env      = __dirname + '/' + confrodo.env + '.json'
-  , config   = confrodo(common, env, 'ENV', 'ARGV')
+  , config   = confrodo(defaults, env, 'ENV', 'ARGV')
   ;
 
 module.exports = config;
