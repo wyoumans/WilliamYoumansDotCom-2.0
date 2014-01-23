805ff919a54d83716de0b1cbaed71d2b2be13205
William Youmans <will@williamyoumans.com>
2014-01-23 11:01:57 -0700

    added tools seed script

--- scripts/import-tools.js
+++ scripts/import-tools.js
@@ -1,28 +1,32 @@
 'use strict';
 
-var logger = require('../lib').logger
-  , tools  = require('./data/tools')
-  , Tool   = require('../models').Tool
-  , async  = require('async')
+var logger   = require('../lib').logger
+  , tools    = require('./data/tools')
+  , Tool     = require('../models').Tool
+  , async    = require('async')
+  , mongoose = require('mongoose')
   ;
 
 (function() {
 
   logger.info('Beginning Tool Import');
 
-  Tool.remove({});
+  mongoose.connection.collections['tools'].drop(function(err) {
+    logger.info('Tools dropped');
 
-  async.each(Object.keys(tools), function(category, done) {
-    async.each(tools[category], function(tool, finish) {
-      tool.category = category;
-      new Tool(tool).save(finish);
-    }, done);
-  }, function(err) {
-    if (err) {
-      logger.error(err);
-    }
+    async.each(Object.keys(tools), function(category, done) {
+      async.each(tools[category], function(tool, finish) {
+        tool.category = category;
+        logger.info('inserting ' + tool.name);
+        new Tool(tool).save(finish);
+      }, done);
+    }, function(err) {
+      if (err) {
+        logger.error(err);
+      }
 
-    logger.info('Tool Import Complete');
-    process.exit();
+      logger.info('Tool Import Complete');
+      process.exit();
+    });
   });
 })();
