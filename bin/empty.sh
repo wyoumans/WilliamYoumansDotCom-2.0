4ba29174b866bdc57b5cbd39ec1621a3a06fc0b3
William Youmans <will@williamyoumans.com>
2014-07-04 19:17:02 -0600

    updated database seed

--- bin/empty.sh
+++ bin/empty.sh
@@ -3,3 +3,6 @@
   echo ""
   echo "emptying database"
   mongo williamyoumans --eval "db.dropDatabase()";
+  mongo williamyoumans_local --eval "db.dropDatabase()";
+  mongo williamyoumans_staging --eval "db.dropDatabase()";
+  mongo williamyoumans_production --eval "db.dropDatabase()";
--- bin/populate.sh
+++ bin/populate.sh
@@ -4,10 +4,13 @@ echo ""
 echo "beginning database population"
 echo ""
 
+echo "seeding redirects..."
 node scripts/seed-redirects.js
 echo ""
+echo "fetching images from instagram..."
 node scripts/fetch-images.js
 echo ""
+echo "fetching tracks from last.fm..."
 node scripts/fetch-tracks.js
 
 echo ""
