4493673b766849fd976ab2ae61639cbaaf66ec29
William Youmans <will@williamyoumans.com>
2014-12-16 20:42:13 -0500

    compiled assets

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ var defaults = {
     "userid": process.env.INSTAGRAM_USERID
   },
   "twitter": {
-    "consumer_key": process.env.TWITTER_CONSUMER,
+    "consumer_key": process.env.TWITTER_CONSUMER_KEY,
     "consumer_secret": process.env.TWITTER_CONSUMER_SECRET,
     "token": process.env.TWITTER_TOKEN,
     "token_secret": process.env.TWITTER_TOKEN_SECRET,
