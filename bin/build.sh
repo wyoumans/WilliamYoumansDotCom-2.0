7e77878b12b00e05bcc77c2d9a6253fb57cd6451
William Youmans <will@williamyoumans.com>
2015-09-29 16:52:20 -0400

    added js to build script

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,5 @@
 # Ensure the latest assets have been compiled
 make compass
 
-# Concatenation and minify js
-#  public/bower_components/jquery.easing/js/jquery.easing.js,
-#  public/bower_components/snabbt.js/snabbt.js,
-#  public/scripts/custom.js
-
 # Minify the assets
 node ./bin/minify
