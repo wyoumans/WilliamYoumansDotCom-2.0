34b815546da663a589fdc4e2181720c96dec796e
William Youmans <will@williamyoumans.com>
2015-10-01 22:53:33 -0400

    stop fetching and displaying tracks

--- scripts/fetch-tweets.js
+++ scripts/fetch-tweets.js
@@ -4,7 +4,7 @@ var Twitter = require('node-twitter')
   , config  = require('../config')
   , logger  = require('../lib').logger
   , Tweet   = require('../models').Tweet
-  , async  = require('async')
+  , async   = require('async')
   , _       = require('lodash')
   ;
 
