39fa38aa23102751a1a0d8402126f2a688d87aeb
William Youmans <will@williamyoumans.com>
2015-10-30 12:52:52 -0400

    footer overhaul with new navigation

--- controllers/sitemap.js
+++ controllers/sitemap.js
@@ -15,7 +15,21 @@ function getSitemap(req, res) {
     '/projects',
     '/about',
     '/contact',
-    '/blog/detail/part-1-interactive-javascript-map-of-canada-with-raphael'
+    '/blog/detail/part-1-interactive-javascript-map-of-canada-with-raphael',
+
+    // SEO pages
+    '/charlotte-startup-consulting',
+    '/responsive-website-development',
+    '/hand-crafted-website-development',
+    '/charlotte-north-carolina-web-development-services',
+    '/custom-web-applications',
+    '/php-laravel-development',
+    '/custom-wordpress-themes',
+    '/node-js-custom-web-application-development',
+    '/php-laravel-development',
+    '/custom-ecommerce-solutions',
+    '/north-carolina-website-consulting',
+    '/small-business-marketing-websites'
   ];
 
   res.setHeader('Content-Type', 'application/xhtml+xml');
