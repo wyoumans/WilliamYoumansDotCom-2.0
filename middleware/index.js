d9c2304c350e57a43614f019efd50b84611e98ff
William Youmans <will@williamyoumans.com>
2015-11-03 12:19:19 -0500

    moved locals to middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports = {
-  errorHandler : require('./error-handler'),
-  redirects    : require('./redirects'),
-  cleanUrl     : require('./clean-url'),
-  cacheControl : require('./cache-control')
+  errorHandler     : require('./error-handler'),
+  redirects        : require('./redirects'),
+  cleanUrl         : require('./clean-url'),
+  cacheControl     : require('./cache-control'),
+  locals           : require('./locals')
 };
