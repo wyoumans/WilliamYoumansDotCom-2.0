ccba5122aa84e54cce4a4146d4d2aa48b5a73abb
William Youmans <will@williamyoumans.com>
2015-11-03 10:50:13 -0500

    handle query strings in the trailing slash redirect

--- middleware/clean-url.js
+++ middleware/clean-url.js
@@ -3,8 +3,9 @@
 module.exports = function() {
   return function(req, res, next) {
 
-    if (req.url.substr(-1) == '/' && req.url.length > 1) {
-      res.redirect(301, req.url.slice(0, -1));
+    if (req.path.substr(-1) == '/' && req.path.length > 1) {
+      var query = req.url.slice(req.path.length);
+      res.redirect(301, req.path.slice(0, -1) + query);
     } else {
       next();
     }
