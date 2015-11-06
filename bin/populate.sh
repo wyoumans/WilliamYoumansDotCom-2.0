8dba915b4c3c120b97445d22f509a85dfe15174b
William Youmans <will@williamyoumans.com>
2015-11-06 16:13:05 -0500

    commenting out services pages for now

--- bin/populate.sh
+++ bin/populate.sh
@@ -7,17 +7,17 @@ echo ""
 echo "seeding redirects..."
 node scripts/seed-redirects.js
 
-echo ""
-echo "seeding services..."
-node scripts/seed-services.js
+# echo ""
+# echo "seeding services..."
+# node scripts/seed-services.js
 
 echo ""
 echo "fetching images from instagram..."
 node scripts/fetch-images.js
 
-echo ""
-echo "fetching tracks from last.fm..."
-node scripts/fetch-tracks.js
+# echo ""
+# echo "fetching tracks from last.fm..."
+# node scripts/fetch-tracks.js
 
 echo ""
 echo "fetching tweets from twitter..."
