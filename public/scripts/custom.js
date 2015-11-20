37b73bd6f7567bb75642d3fa742491ba732435d3
William Youmans <will@williamyoumans.com>
2015-11-20 03:06:58 -0500

    showing hero at full window height

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -5,7 +5,7 @@ $(document).foundation();
 $(function() {
 
   var animationSpeed = 1000,
-      endPosition    = 300,
+      endPosition    = 0,
       easing         = 'easeOutBack',
       $topText       = $('.animated-heading h1 span.top'),
       $bottomText    = $('.animated-heading h1 span.bottom');
@@ -32,13 +32,14 @@ $(function() {
     typing: function(cb) {
       cb = cb || function() {};
 
+      animation.showArrow();
+
       $("#typed-text").typed({
-        strings: ['small business.', 'startup.', 'online store.', 'agency.', 'big idea!'],
+        strings: ['small business.', 'startup.', 'online store.', 'design agency.', 'big idea!'],
         typeSpeed: 50,
         startDelay: 50,
         backSpeed: 0,
-        backDelay: 1000,
-        callback: animation.showArrow
+        backDelay: 1000
       });
     },
     phoneJiggle: function() {
@@ -59,15 +60,41 @@ $(function() {
     }
   };
 
+  // header animations
   if ($('.animated-heading').length) {
+    $('.animation-wrapper').height($(window).innerHeight());
+
     setTimeout(animation.inLeft(animation.inRight(animation.typing)), 1000);
 
     $('a.scroll-arrow').click(function(e) {
       e.preventDefault();
 
-      console.log('SCROLL DOWN!!!');
+      scrollToDiv('#main-nav', true);
     });
   }
 
+  // phone jiggle
   animation.phoneJiggle();
 });
+
+var scrollToDiv = function(selector, withAnimation, cb) {
+  cb = cb || function() {};
+
+  var $page  = $('html, body'),
+      $div   = $(selector).first(),
+      top    = 0,
+      offset = $div.offset();
+
+  if (offset) {
+    top = offset.top;
+  }
+
+  if (withAnimation) {
+    $page.animate({
+      scrollTop: top
+    }, 'fast', cb);
+  } else {
+    $page.scrollTop(top);
+    cb();
+  }
+}
