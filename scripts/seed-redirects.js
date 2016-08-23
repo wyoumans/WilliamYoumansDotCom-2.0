dcfb7f97ed7645a9872e9c434e48bc3409428b78
William Youmans <will@williamyoumans.com>
2016-08-23 09:07:51 -0400

    prevent dropping redirects twice

--- scripts/seed-redirects.js
+++ scripts/seed-redirects.js
@@ -14,20 +14,17 @@ var logger    = require('../lib').logger
   mongoose.connection.collections['redirects'].drop(function(err) {
     logger.info('Redirects dropped');
 
-    Redirect.remove({}, function() {
+    async.each(redirects, function(redirect, done) {
+      logger.info('inserting ' + redirect.before);
+      new Redirect(redirect).save(done);
 
-      async.each(redirects, function(redirect, done) {
-        logger.info('inserting ' + redirect.before);
-        new Redirect(redirect).save(done);
+    }, function(err) {
+      if (err) {
+        logger.error(err);
+      }
 
-      }, function(err) {
-        if (err) {
-          logger.error(err);
-        }
-
-        logger.info('redirect Import Complete');
-        process.exit();
-      });
+      logger.info('redirect Import Complete');
+      process.exit();
     });
   });
 })();
