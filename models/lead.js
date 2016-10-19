24ee67ba155949f0df6bb6f00867be2fb3c7f932
William Youmans <will@williamyoumans.com>
2016-10-19 13:45:16 -0400

    fixed lead model

--- models/lead.js
+++ models/lead.js
@@ -3,7 +3,7 @@
 var Schema = require('mongoose').Schema
   ;
 
-var ContactSchema = new Schema({
+var LeadSchema = new Schema({
   name: {
     type: String,
     required: true
@@ -20,8 +20,8 @@ var ContactSchema = new Schema({
 });
 
 // validate email address
-ContactSchema.path('email').validate(function(value) {
+LeadSchema.path('email').validate(function(value) {
   return /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/i.test(value);
 }, 'invalid email');
 
-module.exports = ContactSchema;
+module.exports = LeadSchema;
